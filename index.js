7e941d905f788b8f423c0270ed7861494c4b48a5
Will <yh@fir.im>
2016-02-28 19:35:55 +0800

    初始化bitbucket-api项目&&add mocha

---
+++ index.js
@@ -0,0 +1,1 @@
+exports.Auth = require("./lib/auth.js")
---
+++ test/auth.js
@@ -0,0 +1,21 @@
+var Auth = require("../index").Auth
+var assert = require('assert');
+var auth;
+var client_id = "S6vLxDGqKUuNG68Sr3", secret = "MjNCNNvjV3BTeMyCm6TxQZLpvSTjaUgZ" ;
+beforeEach(function() {
+  return auth = new Auth(client_id, secret)
+});
+
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function (done) {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+      auth.get_access_token("DwJTvQ4YQv4ky8jmyW",function(err, res, body){
+
+        console.log(err, res, body)
+        done()
+      })
+    });
+  });
+});
